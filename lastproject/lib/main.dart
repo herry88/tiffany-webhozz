e18d3c09f96598ec99744169a92d2dbe0fc8ea7e
Herry Prasetyo <herry081288@gmail.com>
2022-01-22 15:48:48 +0700

    tugas

--- lastproject/lib/main.dart
+++ lastproject/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:lastproject/screen/homepage.dart';
+import 'package:lastproject/screen/dashboard.dart';
+import 'package:lastproject/screen/postscreen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: MyHomePage(),
+      home: DashBoard(),
     );
   }
 }
